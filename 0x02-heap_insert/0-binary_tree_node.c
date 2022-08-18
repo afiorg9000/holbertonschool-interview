6541f7d0aa1ee452a85aa43074e474c6e617b86b
afiorg9000 <3533@holbertonschool.com>
2022-08-18 10:19:55 -0400

    binary tree

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,10 +1,17 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - creates a binary tree node
+ *
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ * Return: pointer to the new node, or NULL on failure
+ */
+
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node = NULL;
-    tmp = (*node)malloc(sizeof(binary_tree_t));
+    binary_tree_t *node = malloc(sizeof(binary_tree_t));
     node->n = value;
     node->parent = parent;
     node->left = node->right = NULL;
--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,11 +1,8 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 #include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -14,16 +11,16 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
 
-typedef struct binary_tree_s heap_t;
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-heap_t *heap_insert(heap_t **root, int value);
 
-#endif /* BINARY_TREES_H */
+
+#endif /* _BINARY_TREES_H_ */
