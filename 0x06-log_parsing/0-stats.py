ae8fa5cb9dd64f47fafa40ac5b238bcf2183cf11
afiorg9000 <3533@holbertonschool.com>
2022-09-19 13:18:54 -0400

    checker correct

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,41 +2,43 @@
 """reads stdin line by line and computes metrics:"""
 import sys
 
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+if __name__ == '__main__':
+    status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+    }
+    count = 1
+    size = 0
 
+    def parse_line(line):
+        """parse line"""
+        try:
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in status_codes.keys():
+                status_codes[status_code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
 
-def logs():
-    """Prints the accumulated logs"""
-    print("File size: {}".format(size))
-    for status in sorted(status_codes.keys()):
-        if status_codes[status]:
-            print("{}: {}".format(status, status_codes[status]))
+    def logs():
+        """prints the accumulated logs"""
+        print("File size: {}".format(size))
+        for key in sorted(status_codes.keys()):
+            if status_codes[key]:
+                print("{}: {}".format(key, status_codes[key]))
 
-
-if __name__ == "__main__":
-    count = 0
-    accumulated_logs = 0
     try:
         for line in sys.stdin:
-            try:
-                line = line.split()
-                accumulated_logs += int(line[-1])
-                if line[-2] in status_codes:
-                    status_codes[line[-2]] += 1
-            except Exception:
-                pass
-            if count == 9:
+            size += parse_line(line)
+            if count % 10 == 0:
                 logs()
-                count = -1
             count += 1
     except KeyboardInterrupt:
         logs()
