b104c4c153dfb6c1f8b37f0a07cab4c107468248
afiorg9000 <3533@holbertonschool.com>
2022-10-12 16:44:29 -0400

    initial commit

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,117 @@
+#include "slide_line.h"
+
+/**
+* right_slider - slides right
+* @line: Pointer
+* @size: size of array
+* Return: 1 upon success, or 0 upon failure 
+*/
+int right_slider(int *line, int size)
+{
+	int i, j, x = 0, temp;
+
+	temp = line[size - 1];
+	for (i = size - 1; i >= 0; i--)
+	{
+		for (j = i - 1; j >= 0; j--)
+		{
+			if (line[j] != 0)
+			{
+				if (temp == line[j])
+				{
+					temp = temp + line[j];
+					line[j] = 0;
+					break;
+				}
+				else if (temp == 0)
+				{
+					temp = line[j];
+					line[i] = temp;
+					line[j] = 0;
+				}
+				else
+				{
+					temp = line[i];
+					x = 1;
+					break;
+				}
+			}
+		}
+		line[i] = temp;
+		if (x == 1)
+		{
+			temp = line[i - 1];
+			x = 0;
+		}
+		else
+			temp = 0;
+	}
+	return (1);
+}
+
+/**
+ * move_left - slides left
+ * @line: Pointer
+ * @size: size of array
+ * Return: 1 upon success, or 0 upon failure  
+*/
+int left_slider(int *line, int size)
+{
+	int i, j;
+	int temp = line[0], x = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = i + 1; j < size; j++)
+		{
+			if (line[j] != 0)
+			{
+				if (temp == line[j])
+				{
+					temp = temp + line[j];
+					line[j] = 0;
+					break;
+				}
+				else if (temp == 0)
+				{
+					temp = line[j];
+					line[i] = temp;
+					line[j] = 0;
+				}
+				else
+				{
+					temp = line[i];
+					x = 1;
+					break;
+				}
+			}
+		}
+		line[i] = temp;
+		if (x == 1)
+		{
+			temp = line[i + 1];
+			x = 0;
+		}
+		else
+			temp = 0;
+	}
+	return (1);
+}
+
+/**
+* slide_line -hat slides and merges an array of integers 
+* @line: pointer
+* @size: size of the array
+* @direction: left or right
+* Return: 1 upon success, or 0 upon failure 
+*/
+int slide_line(int *line, size_t size, int direction)
+{
+	if (line == NULL)
+		return (0);
+	if (direction == SLIDE_LEFT)
+		return (move_left(line, (int)size));
+	if (direction == SLIDE_RIGHT)
+		return (move_right(line, (int)size));
+	return (0);
+}
