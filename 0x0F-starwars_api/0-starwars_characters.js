6ba976f6e77a5dbf3b960c4b5bd7bd721f8ee4a1
afiorg9000 <3533@holbertonschool.com>
2022-12-12 13:51:20 -0400

    checker

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,15 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
 
-request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`).then(res => {
-        res.data.characters.forEach(async(data) => {
-            await request.get(data)
-                .then(res => {
-                    console.log(res.data.name);
+request(url, async function(error, response, body) {
+    if (error) {
+        console.log(error);
+    } else {
+        const characters = JSON.parse(body).characters;
+        for (const character of characters) {
+            const res = await new Promise((resolve, reject) => {
+                request(character, (error, res, html) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(JSON.parse(html).name);
+                    }
                 });
-        });
-    })
-    .catch(err => {
-        console.log('Error:', err);
-    });
+            });
+            console.log(res);
+        }
+    }
+});
