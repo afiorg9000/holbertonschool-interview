e9e8f8bbce1ba558109d6c21fba2ebc80c3d3db7
afiorg9000 <3533@holbertonschool.com>
2022-12-22 14:19:05 -0400

    check

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import requests
+
+
+def count_words(subreddit, word_list):
+    """Counts the number of times a word appears in a subreddit"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) \
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    params = {'limit': 100}
+    r = requests.get(url, headers=headers, params=params,
+                     allow_redirects=False)
+    if r.status_code == 200:
+        data = r.json().get('data')
+        children = data.get('children')
+        titles = [child.get('data').get(
+            'title').lower().split() for child in children]
+        words = [word for title in titles for word in title]
+        word_count = {word: words.count(word) for word in word_list}
+        word_count = {k: v for k, v in sorted(word_count.items(),
+                                              key=lambda item: (-item[1],
+                                                                item[0]))}
+        for k, v in word_count.items():
+            if v > 0:
+                print("{}: {}".format(k, v))
+    else:
+        return None
