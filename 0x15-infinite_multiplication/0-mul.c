2b29bbda3bf2999390e5823f33232f4f6a85e90e
afiorg9000 <3533@holbertonschool.com>
2023-01-11 14:19:09 -0400

    initial

---
+++ 0x15-infinite_multiplication/0-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two positive numbers:
+ * @a: int
+ * @b: int
+ * Return: multiplies two positive numbers:
+ */
+int mul(int a, int b)
+{
+	int c;
+	c = a * b;
+	return (c);
+}
