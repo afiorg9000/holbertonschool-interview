201cb12e44d697cc43689ce5c5bb1e9ad1d90496
afiorg9000 <3533@holbertonschool.com>
2023-01-25 18:39:29 -0400

    che

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+* find_listint_loop - Finds the loop in a linked list.
+* @head: Pointer to the head of list.
+* Return: pointer or null
+*/
+listint_t *find_listint_loop(listint_t *head)
+{
+listint_t *nextNode;
+listint_t *prev;
+nextNode = head;
+
+prev = head;
+while (head && nextNode && nextNode->next)
+{
+head = head->next;
+nextNode = nextNode->next->next;
+if (head == nextNode)
+{
+head = prev;
+prev =  nextNode;
+while (1)
+{
+nextNode = prev;
+while (nextNode->next != head && nextNode->next != prev)
+{
+nextNode = nextNode->next;
+}
+if (nextNode->next == head)
+break;
+head = head->next;
+}
+return (nextNode->next);
+}
+}
+return (NULL);
+}
