edf53397641a3c65be8467312b7df3031bbab38f
afiorg9000 <3533@holbertonschool.com>
2023-01-26 23:20:20 -0400

    che

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -6,26 +6,32 @@
 */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *fast, *slow;
+listint_t *nextNode;
+listint_t *prev;
+nextNode = head;
 
-	if (!head)
-		return (NULL);
-	fast = slow = head;
-	while (slow && fast && fast->next)
-	{
-		slow = slow->next;
-		fast = (fast->next)->next;
-		if (fast == slow)
-		{
-			slow = head;
-			while (slow && fast)
-			{
-				if (slow == fast)
-					return (slow);
-				slow = slow->next;
-				fast = fast->next;
-			}
-		}
-	}
-	return (NULL);
+prev = head;
+while (head && nextNode && nextNode->next)
+{
+head = head->next;
+nextNode = nextNode->next->next;
+if (head == nextNode)
+{
+head = prev;
+prev =  nextNode;
+while (1)
+{
+nextNode = prev;
+while (nextNode->next != head && nextNode->next != prev)
+{
+nextNode = nextNode->next;
 }
+if (nextNode->next == head)
+break;
+head = head->next;
+}
+return (nextNode->next);
+}
+}
+return (NULL);
+}ec
