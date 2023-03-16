ab4e829367085125bf6f354d126401f49cdc806f
afiorg9000 <3533@holbertonschool.com>
2023-03-16 17:10:08 -0400

    double check

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -6,12 +6,17 @@ def makeChange(coins, total):
     """Return: fewest number of coins needed to meet total"""
     if total <= 0:
         return 0
-    coins.sort(reverse=True)
-    count = 0
-    for coin in coins:
-        if coin <= total:
-            count += total // coin
-            total %= coin
-    if total > 0:
+    # Initialize array with a value greater than total
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    # Iterate through all coin values
+    for c in coins:
+        # Update array for multiples of current coin value
+        for i in range(c, total + 1):
+            dp[i] = min(dp[i], dp[i - c] + 1)
+
+    if dp[total] > total:
         return -1
-    return count
+    else:
+        return dp[total]
