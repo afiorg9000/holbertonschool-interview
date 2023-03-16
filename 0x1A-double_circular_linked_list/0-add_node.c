748bfe1c3c673f4f533b381d7caa08c3de1966a6
afiorg9000 <3533@holbertonschool.com>
2023-03-16 17:39:58 -0400

    Betty coding style

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -10,33 +10,33 @@
  */
 List *add_node_end(List **list, char *str)
 {
-    List *new_node;
+List *new_node;
 
-    if (!list || !str)
-        return (NULL);
+if (!list || !str)
+return (NULL);
 
-    new_node = malloc(sizeof(List));
-    if (!new_node)
-        return (NULL);
+new_node = malloc(sizeof(List));
+if (!new_node)
+return (NULL);
 
-    new_node->str = strdup(str);
-    if (!new_node->str) {
-        free(new_node);
-        return (NULL);
-    }
+new_node->str = strdup(str);
+if (!new_node->str) {
+free(new_node);
+return (NULL);
+}
 
-    if (!*list) {
-        new_node->prev = new_node;
-        new_node->next = new_node;
-        *list = new_node;
-    } else {
-        new_node->prev = (*list)->prev;
-        new_node->next = *list;
-        (*list)->prev->next = new_node;
-        (*list)->prev = new_node;
-    }
+if (!*list) {
+new_node->prev = new_node;
+new_node->next = new_node;
+*list = new_node;
+} else {
+new_node->prev = (*list)->prev;
+new_node->next = *list;
+(*list)->prev->next = new_node;
+(*list)->prev = new_node;
+}
 
-    return (new_node);
+return (new_node);
 }
 
 /**
@@ -47,32 +47,32 @@ List *add_node_end(List **list, char *str)
  */
 List *add_node_begin(List **list, char *str)
 {
-    List *new_node;
+List *new_node;
 
-    if (!list || !str)
-        return (NULL);
+if (!list || !str)
+return (NULL);
 
-    new_node = malloc(sizeof(List));
-    if (!new_node)
-        return (NULL);
+new_node = malloc(sizeof(List));
+if (!new_node)
+return (NULL);
 
-    new_node->str = strdup(str);
-    if (!new_node->str) {
-        free(new_node);
-        return (NULL);
-    }
+new_node->str = strdup(str);
+if (!new_node->str) {
+free(new_node);
+return (NULL);
+}
 
-    if (!*list) {
-        new_node->prev = new_node;
-        new_node->next = new_node;
-    } else {
-        new_node->prev = (*list)->prev;
-        new_node->next = *list;
-        (*list)->prev->next = new_node;
-        (*list)->prev = new_node;
-    }
+if (!*list) {
+new_node->prev = new_node;
+new_node->next = new_node;
+} else {
+new_node->prev = (*list)->prev;
+new_node->next = *list;
+(*list)->prev->next = new_node;
+(*list)->prev = new_node;
+}
 
-    *list = new_node;
+*list = new_node;
 
-    return (new_node);
+return (new_node);
 }
