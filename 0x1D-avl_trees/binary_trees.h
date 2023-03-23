e26428375f6190b1311c311309433798fd4dacc1
afiorg9000 <3533@holbertonschool.com>
2023-03-23 14:54:31 -0400

    check

--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -1,9 +1,12 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
-#include <stddef.h>
-#include <stdlib.h>
+#include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -14,17 +17,20 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
-
 typedef struct binary_tree_s avl_t;
 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
+
+
 int binary_tree_is_avl(const binary_tree_t *tree);
-void binary_tree_print(const binary_tree_t *);
+int avl_check(const binary_tree_t *tree, int min, int max, int *height);
 
 #endif /* _BINARY_TREES_H_ */
