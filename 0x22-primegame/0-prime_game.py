c13bea78d6e40ceb9ad5f494b7124c18e1ff4788
afiorg9000 <3533@holbertonschool.com>
2023-05-03 09:45:25 -0400

    prime

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -1,42 +1,55 @@
 #!/usr/bin/python3
+"""
+Contains method to determine the winner of a game
+of prime numbers.
+"""
+
+
+def prime_numbers_between(n):
+    """
+    calculate prime numbers between 1 and n
+
+    Args:
+        n (int): the number to calculate prime numbers up to
+
+    Returns:
+        int: the number of prime numbers between 1 and n
+    """
+    prime_numbers = 0
+
+    for i in range(2, n + 1):
+        is_prime = True
+        for j in range(2, i // 2 + 1):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            prime_numbers += 1
+    return prime_numbers
 
 
 def isWinner(x, nums):
-    def isPrime(num):
-        if num < 2:
-            return False
-        for i in range(2, int(num**0.5)+1):
-            if num % i == 0:
-                return False
-        return True
+    """
+    Determines the winner of a game of prime numbers.
 
-    def playGame(n):
-        primes = [num for num in range(2, n+1) if isPrime(num)]
-        numMoves = 0
-        while True:
-            if numMoves % 2 == 0:
-                player = "Maria"
-            else:
-                player = "Ben"
-            if not primes:
-                return player
-            for prime in primes:
-                if n % prime == 0:
-                    primes.remove(prime)
-            numMoves += 1
+    Args:
+        x (int): the number of rounds to play
+        nums (list): the list of numbers n to play
 
-    mariaWins = 0
-    benWins = 0
-    for i in range(x):
-        result = playGame(nums[i])
-        if result == "Maria":
-            mariaWins += 1
-        elif result == "Ben":
-            benWins += 1
-
-    if mariaWins > benWins:
-        return "Maria"
-    elif benWins > mariaWins:
-        return "Ben"
-    else:
+    Returns:
+        string: the winner of the game (Ben or Maria)
+    """
+    if not x or not nums:
         return None
+    ben = 0
+    maria = 0
+    for i in range(x):
+        prime_nums = prime_numbers_between(nums[i])
+        if prime_nums % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben == maria:
+        return None
+    winner = "Ben" if ben > maria else "Maria"
+    return winner
